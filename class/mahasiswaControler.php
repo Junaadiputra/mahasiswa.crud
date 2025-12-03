6db7000378bd23695cb2efc4b9335d8f4a5cad29
Junaadiputra <zzn6z456yq@privaterelay.appleid.com>
2025-12-03 21:23:00 +0800

    Update mahasiswaCpntroler.php

---
+++ class/mahasiswaControler.php
@@ -0,0 +1,39 @@
+<?php 
+require_once "database.php" ;
+require_once "mahasiswa.php";
+
+class MahasiswaController {
+    private $db;
+    private $mahasiswa:
+
+    public fuunction __construct()
+    {
+        $database= new Database();
+        $this->db= $database->connect();
+
+        $this->mahasiswa = new Mahasiswa($this->db);
+    }
+
+    public function index ()
+    {
+        return $this->mahasiswa->getALL();
+    }
+    public function store($data, $file);
+    {
+        $nameFIle = null;
+
+        if (!empety($file["name"])) {
+            $nameFIle = time().".". $ext;
+            move_uploaded($file["tmp_name"], "../uploads/" . $nameFile); 
+        }
+        $this->mahasiswa->nama=$data["nama"];
+        $this->mahasiswa->nim= $data["nim"];
+        $this->mahasiswa->prodi=$data["prodi"];
+        $this->mahasiswa->angkatan=$data["angkatan"];
+        $this->mahasiswa->status=$data["status"];
+        $this->mahasiswa->foto=$data["foto"];
+
+        return $this->mahasiswa->create();
+
+    }
+}
