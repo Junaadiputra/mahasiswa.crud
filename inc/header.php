acae817bcdbdb59bdf0de1ca34f004b84f36699f
Junaadiputra <zzn6z456yq@privaterelay.appleid.com>
2025-12-04 23:07:05 +0800

    Update header.phpp

--- inc/header.php
+++ inc/header.php
@@ -4,3 +4,20 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Aplikasi CRUD Mahasiswa</title>
+
+        <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+        }
+
+        table {
+            border-collapse: collapse;
+            margin-top: 15px;
+        }
+
+        table th, table td {
+            border: 1px solid #000;
+            padding: 8px 10px;
+        }
+        a.btn {
