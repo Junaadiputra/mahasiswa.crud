2715960afe93afd1356d1322bf04b65a816e5210
Junaadiputra <zzn6z456yq@privaterelay.appleid.com>
2025-12-04 20:52:54 +0800

    Update create.php

--- inc/create.php
+++ inc/create.php
@@ -3,38 +3,40 @@ require_once "../class/mahasiswaControler.php";
 
 $controler=new MahasiswaControler();
 
-if ($_SERVER["REWUEST_METHOD"]== "POST") {
-    $controller->store($_POST, $_FILES["FOTO"]);
-    header("location :index.php");
+if ($_SERVER["REQUEST_METHOD"]== "POST") {
+    $controler->store($_POST, $_FILES["foto"]);
+    header("location: index.php");
     exit;
 }
 ?>
 
-<?php include "header.php;" ?>
-<h2>Tambah Data Mahasiswa</h2>
+<?php include "header.php"; ?>
+    <h2>Tambah Data Mahasiswa</h2>
 
-<form action= "" method="POST" enctype="multipart/form-data">
-    <label>Nama: </label>
-    <input type= "text" name="name" required><br><br>
+    <form action="" method="POST" enctype="multipart/form-data">    
 
-    <label>NIM: </label>
-    <input type="text" name="nim" required><br><br>
+            <label>Nama:</label>
+            <input type="text" name="nama" required><br><br>
 
-    <label>prodi: </label>
-    <input type ="text"name="prodi" required><br><br>
+            <label>NIM:</label>
+            <input type="text" name="nim" required><br><br>
 
-    <label>Angkatan: </label>
-    <input type ="text"name="Angkatan"required><br><br>
+            <label>Prodi:</label>
+            <input type="text" name="prodi" required><br><br>
 
-    <label>Status: </label>
-    <input type ="text"name="Status"required><br><br>
+            <label>Angkatan:</label>
+            <input type="number" name="angkatan" required><br><br>
 
-    <label>Foto: </label>
-    <input type ="text"name="foto"required><br><br>
+            <label>Status:</label>
+            <input type="text" name="status" required><br><br>
 
-    <button type="Submit">Simpan</button>
-    <a href="index.php">Kembali</a>
-</form>
+            <label>Foto:</label>
+            <input type="file" name="foto"><br><br>
+
+            <button type="submit">Simpan</button>
+            <a href="index.php">Kembali</a>
+
+    </form>
 
 <?php include "footer.php"; ?>
 
