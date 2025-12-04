6f929bd726ca40bf19ea29254682daeb4174487e
Junaadiputra <zzn6z456yq@privaterelay.appleid.com>
2025-12-04 22:37:52 +0800

    Update mahasiswa.php

--- class/mahasiswa.php
+++ class/mahasiswa.php
@@ -14,7 +14,65 @@ class mahasiswa {
         $this->conn = $db;
     }
     
-    public function getAll()
+         public function getAll()
+    {
+                $query = "SELECT * FROM mahasiswa ORDER BY id DESC";
+                $stmt = $this->conn->prepare($query);
+                $stmt->execute();
+                return $stmt;
+    }
+
+        public function create()
+    {
+        $query = "INSERT INTO mahasiswa (nama, nim, prodi, angkatan, status, foto)
+                  VALUES (:nama, :nim, :prodi, :angkatan, :status, :foto)";
+                  
+                $stmt = $this->conn->prepare($query);
+                $stmt->bindParam(":nama", $this->nama);
+                $stmt->bindParam(":nim", $this->nim);
+                $stmt->bindParam(":prodi", $this->prodi);
+                $stmt->bindParam(":angkatan", $this->angkatan);
+                $stmt->bindParam(":status", $this->status);
+                $stmt->bindParam(":foto", $this->foto);
+
+        return $stmt->execute();
+    }
+
+    public function find($id)
+    {
+            $query = "SELECT * FROM mahasiswa WHERE id = :id LIMIT 1";
+            $stmt = $this->conn->prepare($query);
+            $stmt->bindParam(":id", $id);
+            $stmt->execute();
+
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+    public function update()
+    {
+        $query = "UPDATE mahasiswa SET 
+                    nama = :nama,
+                    nim = :nim,
+                    prodi = :prodi,
+                    angkatan = :angkatan,
+                    status = :status,
+                    foto = :foto
+                  WHERE id = :id";
+
+        $stmt = $this->conn->prepare($query);
+
+        $stmt->bindParam(":nama", $this->nama);
+        $stmt->bindParam(":nim", $this->nim);
+        $stmt->bindParam(":prodi", $this->prodi);
+        $stmt->bindParam(":angkatan", $this->angkatan);
+        $stmt->bindParam(":status", $this->status);
+        $stmt->bindParam(":foto", $this->foto);
+        $stmt->bindParam(":id", $this->id);
+
+        return $stmt->execute();
+    }
+
+
+
     
 }
 
