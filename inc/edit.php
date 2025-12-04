75a3e79f3c46243a2ef9100ccdcdfeeff1a0115d
Junaadiputra <zzn6z456yq@privaterelay.appleid.com>
2025-12-04 20:40:58 +0800

    Update edit.php

--- inc/edit.php
+++ inc/edit.php
@@ -17,35 +17,35 @@ if ($_SERVER["REQUEST_METHOD"]=== "POST") {
 }
 ?>
 
-<?php include "heade.php"; ?>
-<h2>Edit Data Mahasiswa</2>
+<?php include "header.php"; ?>
+<h2>Edit Data Mahasiswa</h2>
 
 <form action="" method="POST" enctype="multipart/form-data">
 
-    <label>Nama:</type>
-    <input type= "text" name="nama"value="<?= $data\['nama']?>" required><br><br>
+    <label>Nama:</label>
+    <input type= "text" name="nama"value="<?= $data['nama']?>" required><br><br>
 
-    <label>NIM:</type>
-    <input type= "text" name="Nim"value="<?= $data\['nim']?>" required><br><br>
+    <label>NIM:</label>
+    <input type= "text" name="nim"value="<?= $data['nim']?>" required><br><br>
 
-    <label>Prodi:</type>
-    <input type= "text" name="Prodi" value="<?=$data\['prodi']?>" required><br><br>
+    <label>Prodi:</label>
+    <input type= "text" name="prodi" value="<?=$data['prodi']?>" required><br><br>
 
     <label>Angkatan:</label>
-    <input type="text" name="Angkatan" value="<?=$data\['angkatan']?>" required><br><br>
+    <input type="text" name="angkatan" value="<?=$data['angkatan']?>" required><br><br>
 
     <label>Status:</label>
-    <input type= "text" name="Angkattan" value="<?=$data\['status']?>" required><br><br>
+    <input type= "text" name="status" value="<?=$data['status']?>" required><br><br>
 
     <label>Foto Lama:</label><br>
-    <?php if ($data\["foto"] !=""): ?>
-        <img src="../uploads/<?=$data\['foto'] ?>" width="80"><br><br>
+   <?php if ($data["foto"] != ""): ?>
+        <img src="../uploads/<?=$data['foto'] ?>" width="80"><br><br>
     <?php else: ?>
         Tidak ada foto<br><br>
     <?php endif ; ?>
 
     <label>Ganti Foto baru: </label>
-    <inpput type="file" name="foto"><br><br>
+    <input type="file" name="foto"><br><br>
 
     <button type = "submit">Update</button>
     <a href="index.php">Kembali</a>
